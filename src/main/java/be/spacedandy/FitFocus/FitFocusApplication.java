4194e22ce640dd153ce1739f627da79318d987ed
TheBonoboShow <62914951+TheBonoboShow@users.noreply.github.com>
2020-12-02 14:35:47 +0100

    1

---
+++ src/main/java/be/spacedandy/FitFocus/FitFocusApplication.java
@@ -0,0 +1,13 @@
+package be.spacedandy.FitFocus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitFocusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitFocusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/spacedandy/FitFocus/FitFocusApplicationTests.java
@@ -0,0 +1,13 @@
+package be.spacedandy.FitFocus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitFocusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
