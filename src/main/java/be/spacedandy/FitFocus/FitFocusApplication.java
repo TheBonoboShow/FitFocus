71e4d10df0fd3925cb1979472a998fbb03b5a621
TheBonoboShow <62914951+TheBonoboShow@users.noreply.github.com>
2021-01-28 16:58:34 +0100

    fix imports main

--- src/main/java/be/spacedandy/FitFocus/FitFocusApplication.java
+++ src/main/java/be/spacedandy/FitFocus/FitFocusApplication.java
@@ -1,12 +1,10 @@
 package be.spacedandy.FitFocus;
 
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
-import it.ozimov.springboot.mail.configuration.EnableEmailTools;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @EnableEncryptableProperties
